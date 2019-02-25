7ef5dad7c468757e6f133cea6849eeef0951f0d0
Valentijn Muijrers <vmuijrers@gmail.com>
2019-02-25 16:39:42 +0100

    Scene setup

---
+++ TestProject/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+    public int maxObjects = 100;
+    public Vector3 area;
+    private GameObject[] objects;
+    private List<GameObject> spawnedObjects = new List<GameObject>();
+
+    private void Start() {
+        objects = Resources.LoadAll<GameObject>("Objects");
+        StartCoroutine(SpawnObjects());
+    }
+
+    IEnumerator SpawnObjects() {
+        int numObjects = 0;
+        while (numObjects < maxObjects) {
+            yield return null;
+            Vector3 position = new Vector3(
+                Random.Range(0.5f * -area.x, 0.5f * area.x),
+                0,
+                Random.Range(0.5f * -area.z, 0.5f * area.z)); 
+            Quaternion rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+            spawnedObjects.Add(Instantiate(objects[Random.Range(0, objects.Length)], position, rotation));
+            numObjects++;
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(Vector3.zero, area);
+    }
+}
